46acd0b0bfa1a0a8ee9ccbe1f9630d710d7dc220
Abhay955528 <pdiwana608@gmail.com>
2023-05-04 17:57:12 +0530

    Send_req/res

--- createServerTask6.js
+++ createServerTask6.js
@@ -1,8 +1,15 @@
 const http = require('http');
 
-const server = http.createServer((req,res)=>{
-    res.end('abhay');
+const server = http.createServer((req, res) => {
+    if (req.url == '/home'){
+        res.write('Welcome home');
+        res.end();
+    } else if (req.url == '/about'){
+        res.write('Welcome to About Us page');
+        res.end();
+    } else if (req.url == '/node'){
+        res.write('Welcome to my Node Js project');
+        res.end();
+    }
 })
-server.listen(4000,'127.0.0.1',() =>{
-    console.log('listening to the port no 8000');
-});
+server.listen(3000,() => console.log('the server is running'));
